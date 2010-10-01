ab49f6cb47e3224a6003cab0e13d07df5b74fdaa
KS Chan <mr.kschan@gmail.com>
2010-10-01 12:55:42 +0800

    pep8 pythonic-ization

--- tropic.py
+++ tropic.py
@@ -1,4 +1,7 @@
-import re, urllib2, random
+#!/usr/bin/python
+import re
+import urllib2
+import random
 
 p_tropic_head = re.compile('<TD width="')
 p_tropic_tail = re.compile('<\/TD><\/TR>')
@@ -9,19 +12,23 @@ p_adj_get = re.compile('>([^<]+)<\/')
 p_adj_skip = re.compile('nbsp')
 
 tropic_cache = []
-tropicurl = 'http://www.hko.gov.hk/prtver/html/docs/informtc/sound/tc_pronunciation2008e.shtml'
+tropicurl = ('http://www.hko.gov.hk/prtver/html'
+             '/docs/informtc/sound/tc_pronunciation2008e.shtml')
 
 adj_cache = []
 adjurl = 'http://www.keepandshare.com/doc/12894/adjective-list?p=y'
 
+
 def get():
-    if len(tropic_cache) == 0: build_cache()
+    if len(tropic_cache) == 0:
+        build_cache()
     return '%s %s' % (random.choice(adj_cache), random.choice(tropic_cache))
-    
+
+
 def build_cache():
     f = urllib2.urlopen(tropicurl)
     for line in f.readlines():
-        if p_tropic_head.search(line) is not None and p_tropic_tail.search(line) is None:
+        if p_tropic_head.search(line) and not p_tropic_tail.search(line):
             m = p_tropic_get.search(line)
             try:
                 m = m.group(1)
@@ -32,11 +39,11 @@ def build_cache():
 
     f = urllib2.urlopen(adjurl)
     for line in f.readlines():
-        if p_adj_head.search(line) is not None: 
+        if p_adj_head.search(line):
             m = p_adj_get.search(line)
             try:
                 m = m.group(1)
-                if p_adj_skip.search(m) is None:
+                if not p_adj_skip.search(m):
                     adj_cache.append(m.lstrip().rstrip('. ').capitalize())
             except:
                 pass
@@ -45,4 +52,3 @@ def build_cache():
 
 for x in xrange(0, 10):
     print get()
-
